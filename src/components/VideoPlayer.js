349370353a95b2998eb5058c4b61861c4906ea40
Nikita <nbomebinomo@gmail.com>
2020-09-02 18:35:44 +0300

    делаю плеер

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../componentCss/player.css';
 
 export default function VideoPlayer(props){
+    let [timeVideo, setTimeVideo] = useState(0)
+
+    function progressUpdate(event){
+        setTimeVideo(event.target.currentTime)
+        console.log(timeVideo);
+    }
+
+    function pauseVideo(event){
+        setTimeVideo(timeVideo)
+    }
+
     return (
         <div>
-            <video></video>
+            <div className="player">
+                <video onTimeUpdate={progressUpdate}>
+                    <source src="/video/videoNM.mp4" type="video/mp4"></source>
+                </video>
+                <div onClick={pauseVideo} className="player-button player-button__pause"></div>
+            </div>
         </div>
     )
 }
