fe3cc0f53ad07b48cc781f2de4a84da155d79a5d
Nikita <nbomebinomo@gmail.com>
2022-05-28 16:01:55 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize({
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: "postgres://uoqdhcgrxklthu:2ef0fb1127bde460b3bd8584b29d4987977f415fae74c4203a317e9cc9af021c@ec2-52-71-69-66.compute-1.amazonaws.com:5432/dduqho72esquu1",
     dialect: 'postgresql',
     logging: false,
     dialectOptions: {
