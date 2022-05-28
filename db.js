314ad06a3e887eaab9ce61ddb74e23c9ac9530de
Nikita <nbomebinomo@gmail.com>
2022-05-28 17:02:14 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-module.exports = new Sequelize({
-    use_env_variable: process.env.DATABASE_URL,
-    dialect: 'postgresql',
-    logging: false,
-    dialectOptions: {
-        ssl: {      /* <----- Add SSL option */
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
-});
+module.exports = new Sequelize(process.env.DATABASE_URL,{dialectOptions: {
+    ssl: {      /* <----- Add SSL option */
+        require: true,
+        rejectUnauthorized: false
+    }
+}})
