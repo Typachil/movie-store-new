c16a1dec8cc5146facac95b1ebd7f52484ab78eb
Nikita <nbomebinomo@gmail.com>
2022-06-15 23:21:41 +0500

    Добавил admin route

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 import React, { useContext } from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { Context } from '../index';
-import { authRoutes, publicRoutes } from '../routes';
+import { authRoutes, publicRoutes, adminRoutes } from '../routes';
 import { HOME_ROUTE } from '../utils/consts';
 
 const AppRouter = observer(() => {
@@ -11,6 +11,9 @@ const AppRouter = observer(() => {
         <div>
             <Switch>
                 {user._isAuth && authRoutes.map(({path, Component}) => 
+                    <Route key={path} path={path} component={Component} exact/>
+                )}
+                {user.user.role == "ADMIN" && adminRoutes.map(({path, Component}) =>
                     <Route key={path} path={path} component={Component} exact/>
                 )}
                 {publicRoutes.map(({path, Component}) => 
